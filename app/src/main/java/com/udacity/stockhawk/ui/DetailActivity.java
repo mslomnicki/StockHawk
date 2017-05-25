94e6b8d1d4130ae7cc290b785b38f5127fe55d8d
Marek Słomnicki <marek@slomnicki.net>
2017-05-25 22:25:49 +0200

    Detail activity

--- app/src/main/java/com/udacity/stockhawk/ui/DetailActivity.java
+++ app/src/main/java/com/udacity/stockhawk/ui/DetailActivity.java
@@ -3,45 +3,53 @@ package com.udacity.stockhawk.ui;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.os.Bundle;
-import android.support.v4.app.LoaderManager;
-import android.support.v4.content.CursorLoader;
-import android.support.v4.content.Loader;
-import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.helper.ItemTouchHelper;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 import com.udacity.stockhawk.R;
 import com.udacity.stockhawk.data.Contract;
-import com.udacity.stockhawk.data.PrefUtils;
-import com.udacity.stockhawk.sync.QuoteSyncJob;
+import com.udacity.stockhawk.data.FormatUtils;
 
+import java.io.IOException;
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.StringTokenizer;
+
+import au.com.bytecode.opencsv.CSVParser;
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import timber.log.Timber;
 
 public class DetailActivity extends AppCompatActivity {
 
+    @SuppressWarnings("WeakerAccess")
     @BindView(R.id.symbol)
-    TextView stockName;
+    TextView mStockName;
 
+    @SuppressWarnings("WeakerAccess")
     @BindView(R.id.price)
-    TextView price;
+    TextView mPrice;
 
+    @SuppressWarnings("WeakerAccess")
     @BindView(R.id.change_percent)
-    TextView percentChange;
+    TextView mPercentChange;
 
+    @SuppressWarnings("WeakerAccess")
     @BindView(R.id.change_absolute)
-    TextView absoluteChange;
+    TextView mAbsoluteChange;
+
+    @SuppressWarnings("WeakerAccess")
+    @BindView(R.id.chart)
+    LineChart mChart;
 
     public static void startActivity(Context context, String stock) {
         Intent intent = new Intent(context, DetailActivity.class);
@@ -57,13 +65,90 @@ public class DetailActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        String stock = getIntent().getStringExtra(Intent.EXTRA_TEXT);
-        if(stock == null || stock.isEmpty()) {
+        String symbol = getIntent().getStringExtra(Intent.EXTRA_TEXT);
+        if (symbol == null || symbol.isEmpty()) {
             finish();
         }
-        getSupportActionBar().setTitle(stock);
-        stockName.setText(stock);
+        getSupportActionBar().setTitle(symbol);
+        initializeFields(symbol);
+    }
 
+    private void initializeFields(String symbol) {
+        Cursor cursor = fetchData(symbol);
+        mStockName.setText(symbol);
 
+        float rawPrice = cursor.getFloat(Contract.Quote.POSITION_PRICE);
+        mPrice.setText(FormatUtils.getDollarFormatter().format(rawPrice));
+
+        float rawPercentChange = cursor.getFloat(Contract.Quote.POSITION_PERCENTAGE_CHANGE);
+        mPercentChange.setText(FormatUtils.getPercentageFormatter().format(rawPercentChange / 100));
+        if (rawPercentChange > 0) {
+            mPercentChange.setBackgroundResource(R.drawable.percent_change_pill_green);
+        } else {
+            mPercentChange.setBackgroundResource(R.drawable.percent_change_pill_red);
+        }
+
+        float rawAbsoluteChange = cursor.getFloat(Contract.Quote.POSITION_ABSOLUTE_CHANGE);
+        mAbsoluteChange.setText(FormatUtils.getDollarWithPlusFormatter().format(rawAbsoluteChange));
+        if (rawAbsoluteChange > 0) {
+            mAbsoluteChange.setBackgroundResource(R.drawable.percent_change_pill_green);
+        } else {
+            mAbsoluteChange.setBackgroundResource(R.drawable.percent_change_pill_red);
+        }
+
+        String historyData = cursor.getString(Contract.Quote.POSITION_HISTORY);
+        cursor.close();
+        initializeChart(symbol, historyData);
+    }
+
+    @NonNull
+    private Cursor fetchData(String symbol) {
+        Cursor cursor = getContentResolver().query(
+                Contract.Quote.makeUriForStock(symbol),
+                Contract.Quote.QUOTE_COLUMNS.toArray(new String[0]),
+                null,
+                null,
+                null);
+        if (cursor == null || cursor.getCount() == 0) finish();
+        cursor.moveToFirst();
+        return cursor;
+    }
+
+    private void initializeChart(String symbol, String historyData) {
+        CSVParser historyParser = new CSVParser();
+        List<Entry> entries = new ArrayList<>();
+        StringTokenizer tokenizer = new StringTokenizer(historyData, "\n");
+        //TODO repair chart
+        long i=0;
+        while (tokenizer.hasMoreTokens()) {
+            try {
+                String nextLine = tokenizer.nextToken();
+                String[] strings = historyParser.parseLine(nextLine);
+                long x = Long.parseLong(strings[0]);
+                float y = Float.parseFloat(strings[1]);
+                x = i++;
+                entries.add(new Entry(x, y));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        mChart.getDescription().setEnabled(false);
+        LineDataSet lineDataSet = new LineDataSet(entries, symbol);
+        LineData data = new LineData(lineDataSet);
+        lineDataSet.setColor(R.color.colorAccent);
+        lineDataSet.setLineWidth(3f);
+        lineDataSet.setDrawCircles(false);
+//        mChart.getXAxis().setValueFormatter(new IAxisValueFormatter() {
+//
+//            private DateFormat mDateInstance = DateFormat.getDateInstance();;
+//
+//            @Override
+//            public String getFormattedValue(float v, AxisBase axisBase) {
+//                long stamp = (long) v;
+//                return mDateInstance.format(new Date(stamp));
+//            }
+//        });
+        mChart.setData(data);
+        mChart.invalidate();
     }
 }
