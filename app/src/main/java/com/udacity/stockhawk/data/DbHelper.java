fe2e4af095b750ed53ae9b6ea09428310515f46e
Marek Słomnicki <marek@slomnicki.net>
2017-05-26 18:23:49 +0200

    DB upgrade script change

--- app/src/main/java/com/udacity/stockhawk/data/DbHelper.java
+++ app/src/main/java/com/udacity/stockhawk/data/DbHelper.java
@@ -35,9 +35,6 @@ class DbHelper extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
-        db.execSQL(" DROP TABLE IF EXISTS " + Quote.TABLE_NAME);
-
-        onCreate(db);
+        // noop on upgrade - still 1st version of database
     }
 }
